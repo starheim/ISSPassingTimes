ae83b6b6d4e03788fb13c09494a4cd292e0925d4
William <wstarvall@gmail.com>
2020-12-27 18:33:00 +0100

    Add iss overhead script

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+import json
+from datetime import datetime
+
+astronauts = "http://api.open-notify.org/astros.json"
+issPass = "http://api.open-notify.org/iss-pass.json"
+
+issPassParams = {
+	"lat":62,
+	"lon":7
+}
+
+astronautsResponse = requests.get(astronauts)
+issPassResponse = requests.get(issPass, params=issPassParams)
+
+passes = issPassResponse.json()['response']
+
+for p in passes:
+	risetime = datetime.fromtimestamp(p['risetime'])
+	duration = p['duration']
+	print(f"ISS will pass over Molde: {risetime} for {duration}s")
+
+
